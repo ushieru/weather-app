04becc81cb84058c91155ad41053d2744210d803
ushieru <uzielcocolan@gmail.com>
2022-05-21 18:24:44 -0500

    Add history

--- pages/api/history.ts
+++ pages/api/history.ts
@@ -1,29 +1,26 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { APIError } from '../../@types/apiError';
-import { Weather } from '../../@types/weather';
+import { IAPIError, APIError } from '../../@types/apiError';
+import { History } from '../../@types/history';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<History | IAPIError>) {
+  if (req.method != 'GET') return res.status(405).json(new APIError(405, 'Method Not Allowed'))
+  const query = req.query.q as string
+  const date = req.query.dt as string
+  if (!query) return res.status(400).json(new APIError(400, 'Missing Paramas'))
+  const url = buildURL(query, date)
+  const response = await fetch(url, options)
+  const jsonRespose: History = await response.json()
+  if (!response.ok) return res.status(400).json(jsonRespose)
+  return res.json(jsonRespose)
+}
 
 const buildURL = (query: string, date: string) =>
-    `https://weatherapi-com.p.rapidapi.com/history.json?q=${query}&dt=${date}`
+  `https://weatherapi-com.p.rapidapi.com/history.json?q=${query}&dt=${date}`
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<Weather | APIError>) {
-    const query = req.query.q as string
-    const date = req.query.dt as string
-    if (!query || !date) return res.status(400).json({
-        error: {
-            code: 400,
-            message: 'Missing Paramas'
-        }
-    })
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
-            'X-RapidAPI-Key': process.env.RAPID_API_KEY as string
-        }
-    }
-    const url = buildURL(query, date)
-    const response = await fetch(url, options)
-    const jsonRespose: Weather = await response.json()
-    if (!response.ok) return res.status(400).json(jsonRespose)
-    return res.json(jsonRespose)
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.RAPID_API_KEY as string
+  }
 }
