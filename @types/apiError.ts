a5571a3cdb21f86233f659ecb59069110980371b
ushieru <uzielcocolan@gmail.com>
2022-05-21 18:25:04 -0500

    Add APIError class

--- @types/apiError.ts
+++ @types/apiError.ts
@@ -1,8 +1,18 @@
-export interface APIError {
+export interface IAPIError {
     error: Error
 }
 
 export interface Error {
     code: number
     message: string
+}
+
+export class APIError implements APIError {
+    error: Error
+    constructor(code: number, message: string) {
+        this.error = {
+            code,
+            message
+        }
+    }
 }
--- pages/api/weather.ts
+++ pages/api/weather.ts
@@ -1,28 +1,25 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { APIError } from '../../@types/apiError';
+import { APIError, IAPIError } from '../../@types/apiError';
 import { Weather } from '../../@types/weather';
 
-const buildURL = (query: string) =>
-  `https://weatherapi-com.p.rapidapi.com/current.json?q=${query}`
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse<Weather | APIError>) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Weather | IAPIError>) {
+  if (req.method != 'GET') return res.status(405).json(new APIError(405, 'Method Not Allowed'))
   const query = req.query.q as string
-  if (!query) return res.status(400).json({
-    error: {
-      code: 400,
-      message: 'Missing Paramas'
-    }
-  })
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
-      'X-RapidAPI-Key': process.env.RAPID_API_KEY as string
-    }
-  }
+  if (!query) return res.status(400).json(new APIError(400, 'Missing Paramas'))
   const url = buildURL(query)
   const response = await fetch(url, options)
   const jsonRespose: Weather = await response.json()
   if (!response.ok) return res.status(400).json(jsonRespose)
   return res.json(jsonRespose)
+}
+
+const buildURL = (query: string) =>
+  `https://weatherapi-com.p.rapidapi.com/current.json?q=${query}`
+
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.RAPID_API_KEY as string
+  }
 }
