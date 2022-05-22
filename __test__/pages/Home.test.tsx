53ecb4975b21f3f9d264afc5d2cc3d9485686ec9
ushieru <uzielcocolan@gmail.com>
2022-05-21 20:18:52 -0500

    Add vitest jsdom test

---
+++ __test__/pages/Home.test.tsx
@@ -0,0 +1,11 @@
+// @vitest-environment jsdom
+
+import { expect, test } from 'vitest'
+import { render, screen, within } from '@testing-library/react'
+import Home from '../../pages'
+
+test('home', () => {
+    render(<Home />)
+    const main = within(screen.getByRole('main'))
+    expect(main.getByRole('heading', { level: 1, name: /Hello world!/i })).toBeDefined()
+})
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  return <>
+    <main>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </main>
+  </>
 }
