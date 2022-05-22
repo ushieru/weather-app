843c2b312c96ec22cd1db2e6978da723f63617ba
ushieru <uzielcocolan@gmail.com>
2022-05-21 20:38:11 -0500

    Fix throw RAPID_API_KEY isn't defined in .env.test

--- __test__/globalSetup/setEnv.ts
+++ __test__/globalSetup/setEnv.ts
@@ -2,4 +2,7 @@ import dotenv from 'dotenv'
 
 export default function setEnv() {
     dotenv.config({ path: '.env.test' })
+    if (!process.env.RAPID_API_KEY) {
+        throw 'RAPID_API_KEY is not defined, You must define it in .env.test'
+    }
 }
