c5463d2b136084e5cb9d87b93144c791af9859f4
Kleinad <juan_mejia13@outlook.com>
2022-05-21 21:54:02 -0500

    Add head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <meta name="theme-color"
+        content="#0A1C2D"
+        media="(prefers-color-scheme: light)" />
+      <meta name="theme-color"
+        content="#0A1C2D"
+        media="(prefers-color-scheme: dark)" />
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
